c0a0ca9dc850a1e153f1315df0896e7b0d0b4fc3
h-rub <rubio@loopgk.com>
2023-10-23 13:14:52 -0600

    se cambió a 8001

--- src/store/slice/ordersSlice.js
+++ src/store/slice/ordersSlice.js
@@ -53,7 +53,7 @@ export const getOpenOrdersList = () => (dispatch) => {
     // };
     // dispatch(addEvent(startFetchOrders));
     axios
-      .get('http://23.100.25.47:8000/api/v1/orders')
+      .get('http://127.0.0.1:8001/api/v1/orders')
       .then((response) => {
         if (response.status === 200) {
           //dispatch(setLoading(false));
--- src/store/slice/testResultSlice.js
+++ src/store/slice/testResultSlice.js
@@ -42,7 +42,7 @@ export const getTestResults = (barcode) => (dispatch) => {
     // };
     // dispatch(addEvent(startFetchOrders));
     axios
-      .get('http://23.100.25.47:8000/api/v1/test-result?barcode='+barcode)
+      .get('http://127.0.0.1:8001/api/v1/test-result?barcode='+barcode)
       .then((response) => {
         if (response.status === 200) {
           //dispatch(setLoading(false));
